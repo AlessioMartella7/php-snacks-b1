568dddd7c1239d72ed9b700a4c57c5ea836a8ff6
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-04 12:10:02 +0200

    added variables to recive GET parameters

--- index.php
+++ index.php
@@ -5,12 +5,11 @@ Con un form passare come parametri GET name, mail e age e verificare (cercando i
 <?php 
 
 
+$userName = $_GET['name'];
+$userAge = $_GET['age'];
+$userEmail = $_GET['email'];
 
-
-
-
-
-
+var_dump($userName)
 
 
  ?>
@@ -30,14 +29,14 @@ Con un form passare come parametri GET name, mail e age e verificare (cercando i
     <main>
         <div class="container">
             <section class="row">
-                <form action="">
+                <form action="index.php" method="get">
                     <div class="input-group mb-3">
                         <input type="text" class="form-control" placeholder="Username" aria-label="Username"
-                            aria-describedby="basic-addon1">
+                            aria-describedby="basic-addon1" name="name">
                         <input type="text" class="form-control" placeholder="Age" aria-label="Username"
-                            aria-describedby="basic-addon1">
+                            aria-describedby="basic-addon1" name="age">
                         <input type="email" class="form-control" placeholder="email" aria-label="Username"
-                            aria-describedby="basic-addon1">
+                            aria-describedby="basic-addon1" name="email">
                     </div>
                     <button type="submit">Accedi</button>
                 </form>

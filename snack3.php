dfa10e8524808a11aeeba516368f14ec53c28681
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-04 15:48:00 +0200

    snack4 milestone 4A

--- snack3.php
+++ snack3.php
@@ -36,7 +36,7 @@ $db = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Snack3</title>
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
